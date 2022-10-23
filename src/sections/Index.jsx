2ad43e2007b6a5a07d0f525d897cb6314486af34
Kiran Kumar <kiran.kumar@medibuddy.in>
2022-10-24 02:43:16 +0530

    ref1

--- src/sections/Index.jsx
+++ src/sections/Index.jsx
@@ -10,37 +10,37 @@ import Blogs from "./Blogs";
 // import Stats from './Stats';
 import Testimonial from "./Testimonial";
 import Contact from "./Contact";
-import Map from "./Map";
+// import Map from "./Map";
 import Footer from "./Footer";
 import Login from "./Login";
 
 export default class Index extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      showUploadPopup: false,
-      uploadCalledForLeadId: null,
-    };
-  }
+ constructor(props) {
+  super();
+  this.state = {
+   showUploadPopup: false,
+   uploadCalledForLeadId: null,
+  };
+ }
 
-  render() {
-    return (
-      <div>
-        <Navigation />
-        <Home />
-        <About />
-        <Team />
-        <Statement />
-        <Service />
-        <Portfolio />
-        <Testimonial />
-        {/* <Stats /> */}
-        <Blogs />
-        <Contact />
-        {/* <Map /> */}
-        <Footer />
-        <Login />
-      </div>
-    );
-  }
+ render() {
+  return (
+   <div>
+    <Navigation />
+    <Home />
+    <About />
+    <Team />
+    <Statement />
+    <Service />
+    <Portfolio />
+    <Testimonial />
+    {/* <Stats /> */}
+    <Blogs />
+    <Contact />
+    {/* <Map /> */}
+    <Footer />
+    <Login />
+   </div>
+  );
+ }
 }
